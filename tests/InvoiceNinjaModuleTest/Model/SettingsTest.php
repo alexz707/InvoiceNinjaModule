6c2301d856345f8344534e76f2e1e568c4b662c0
Alexander Zamponi <azn@connexgroup.net>
2017-03-13 20:02:48 +0100

    added settings test

---
+++ tests/InvoiceNinjaModuleTest/Model/SettingsTest.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace InvoiceNinjaModuleTest\Model;
+
+use InvoiceNinjaModule\Model\Settings;
+use InvoiceNinjaModule\Module;
+
+class SettingsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateEmptySettings()
+    {
+        new Settings([]);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateEmptyToken()
+    {
+        $settings = [
+            Module::TOKEN => '',
+            Module::TOKEN_TYPE => 'testtokentype',
+            Module::API_TIMEOUT => 100,
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateMissingToken()
+    {
+        $settings = [
+            Module::TOKEN_TYPE => 'testtokentype',
+            Module::API_TIMEOUT => 100,
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateEmptyTokenType()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::TOKEN_TYPE => '',
+            Module::API_TIMEOUT => 100,
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateMissingTokenType()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::API_TIMEOUT => 100,
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateEmptyTimeout()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::TOKEN_TYPE => 'testtokentype',
+            Module::API_TIMEOUT => null,
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateNegativeTimeout()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::TOKEN_TYPE => 'testtokentype',
+            Module::API_TIMEOUT => -1,
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateMissingTimeout()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::TOKEN_TYPE => 'testtokentype',
+            Module::HOST_URL => 'http://test.dev',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateEmptyHostUrl()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::TOKEN_TYPE => '',
+            Module::API_TIMEOUT => 100,
+            Module::HOST_URL => '',
+        ];
+
+        new Settings($settings);
+    }
+
+    /**
+     * @expectedException \InvoiceNinjaModule\Exception\InvalidParameterException
+     */
+    public function testCreateMissingHostUrl()
+    {
+        $settings = [
+            Module::TOKEN => 'testtoken',
+            Module::TOKEN_TYPE => 'testtokentype',
+            Module::API_TIMEOUT => 100,
+        ];
+
+        new Settings($settings);
+    }
+}
