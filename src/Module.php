0780c46d354bfe4b73b2f54c94a3a9a87d5466c3
Alexander Zamponi <azn@connexgroup.net>
2017-04-08 01:06:29 +0200

    changed to psr-4

--- src/Module.php
+++ src/Module.php
@@ -22,6 +22,6 @@ class Module implements ConfigProviderInterface
      */
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
