68e043ece356f696d2291e29b6d4dfb9f49b4385
alex <562324+alexz707@users.noreply.github.com>
2021-12-08 14:00:26 +0100

    testing ci

---
+++ .laminas-ci/post-run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# $1 = Exit Status of Job
+# $2 = User
+# $3 = WorkDir
+# $4 = The JOB Json String
+
+# JOB=$4
+# PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
+vendor/bin/ocular code-coverage:upload --repository=g/alexz707/InvoiceNinjaModule --format=php-clover coverage.clover
